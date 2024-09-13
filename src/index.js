941b08be9c20a10cec8429ac0455a1d5eb4d09a1
Bisham6 <83863306+Bisham6@users.noreply.github.com>
2024-09-13 21:39:04 +0530

    code changeess

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client'; // Import from 'react-dom/client'
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Create a root and render the App component
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
 registerServiceWorker();
