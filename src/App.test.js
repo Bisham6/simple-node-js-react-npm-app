819745e425489d0f56e35aa7702bd272004b06dc
Bisham6 <83863306+Bisham6@users.noreply.github.com>
2024-09-13 21:45:46 +0530

    code changeess again

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,14 @@
-import React, {StrictMode} from 'react';
-import ReactDOM from 'react-dom';
+import React, { StrictMode } from 'react';
+import ReactDOM from 'react-dom/client'; // Import from 'react-dom/client'
 import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.createRoot(div).render(
+  
+  // Use createRoot from 'react-dom/client'
+  const root = ReactDOM.createRoot(div);
+  
+  root.render(
     <StrictMode>
       <App />
     </StrictMode>
